cd157c3e2dc237e4388b14fdd341a05d56f697d2
zygvlogs <108675716+zygvlogs@users.noreply.github.com>
2024-08-02 15:18:18 +0300

    Update browser.py

--- browser.py
+++ browser.py
@@ -4,9 +4,14 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
 
 DOMAIN_MAP = {
+    'home.zyg': 'zygvlogs.site/homedex',
     'zyg.zyg': 'zygvlogs.site',
-    #'babrai.babrai': 'babrai.com',
-    'millieb.millie': 'zygvlogs.site/millieb',}
+    'wikipedia.babrai': 'wikipedia.org',
+    'millieb.millie': 'zygvlogs.site/millieb',
+    'rickroll.zyg': 'icegif.com/wp-content/uploads/2023/01/icegif-162.gif',
+    'pinguins.babrai': 'i.pinimg.com/originals/0f/31/53/0f3153d46f659b681ac9b28b8d9872bb.gif',
+    'grass.zyg': 'i.kym-cdn.com/editorials/icons/mobile/000/004/533/tgcover.jpg',
+    }
 
 class SimpleBrowser(QMainWindow):
     def __init__(self):
@@ -29,13 +34,20 @@ class SimpleBrowser(QMainWindow):
         container.setLayout(layout)
         self.setCentralWidget(container)
 
+         # Load home page
+        self.load_home_page()
+
+    def load_home_page(self):
+        self.url_entry.setText("http://home.zyg")
+        self.fetch_content()
+
     def fetch_content(self):
         url = self.url_entry.text().strip()
         parsed_url = QUrl(url)
         full_domain = parsed_url.host()
 
         if full_domain not in DOMAIN_MAP:
-            QMessageBox.critical(self, "Error", "No simulated redirect for this domain")
+            QMessageBox.critical(self, "Error", "Nothing was found or use http://")
             return
 
        
